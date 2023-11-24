2614e2c5aa58f8f3db08c1201554f9cd2d9fc74a
Kumar-Shivam-02 <ksa211@sfu.ca>
2023-11-23 18:17:46 -0800

    basic cleaning

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <nav>
+            <h3 className="nav--title">KS</h3>
+            <h4 className="nav--text">About</h4>
+            <h4 className="nav--text">Skills</h4>
+            <h4 className="nav--text">Experience</h4>
+            <h4 className="nav--text">Contact</h4>
+        </nav>
+    )
+}
