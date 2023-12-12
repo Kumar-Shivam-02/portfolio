f298114aab12b28190c2e865d2e7ba565976e66a
Kumar-Shivam-02 <ksa211@sfu.ca>
2023-12-12 15:48:30 -0800

    removing speed insights

--- src/App.js
+++ src/App.js
@@ -2,11 +2,10 @@
 import './App.css';
 import './mediaqueries.css';
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from "@vercel/speed-insights/next"
-import NavbarDesktop from "./components/NavbarDesktop.js"
-import NavbarMobile from "./components/NavbarMobile.js"
-import Profile from "./components/Profile.js"
-import About from "./components/About.js"
+import NavbarDesktop from "./components/NavbarDesktop.js";
+import NavbarMobile from "./components/NavbarMobile.js";
+import Profile from "./components/Profile.js";
+import About from "./components/About.js";
 import Experience from './components/Experience.js';
 import Projects from './components/Projects.js';
 import Contact from './components/Contact.js';
@@ -24,7 +23,6 @@ function App() {
       <Contact />
       <Footer />
       <Analytics />
-      <SpeedInsights />
     </div>
   );
 }
